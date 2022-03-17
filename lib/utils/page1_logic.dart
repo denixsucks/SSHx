cd5f9e8e6dff4c876f1f082345056f423dddb6cd
denixsucks <erylmzdnz@gmail.com>
2022-03-17 03:05:30 +0300

    new things

--- lib/utils/page1_logic.dart
+++ lib/utils/page1_logic.dart
@@ -3,11 +3,13 @@ import 'dart:io';
 import 'dart:convert';
 
 void main(List<String> args) async {
+  final config_username;
   final socket = await SSHSocket.connect('localhost', 22);
-
+  stdout.write('Username: ');
+  config_username = stdin.readLineSync();
   final client = SSHClient(
     socket,
-    username: 'root',
+    username: config_username,
     onPasswordRequest: () {
       stdout.write('Password: ');
       stdin.echoMode = false;
@@ -15,7 +17,7 @@ void main(List<String> args) async {
     },
   );
 
-  final uptime = await client.run('uptime');
+  final uptime = await client.run('ls');
   print(utf8.decode(uptime));
 
   client.close();
