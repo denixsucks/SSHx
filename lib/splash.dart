2648ba0289e3f3899f79bdf7c7fc2e7245fa8af0
denixsucks <erylmzdnz@gmail.com>
2022-05-24 14:54:08 +0300

    ok nice.

---
+++ lib/splash.dart
@@ -0,0 +1,36 @@
+import 'package:flutnix/loginscreen.dart';
+import 'package:flutter/material.dart';
+
+class Splash extends StatefulWidget {
+  const Splash({Key? key}) : super(key: key);
+
+  @override
+  _SplashState createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  void initState() {
+    super.initState();
+    _navigateToHome();
+  }
+
+  _navigateToHome() async {
+    await Future.delayed(const Duration(milliseconds: 2000), () {});
+    Navigator.pushReplacement(
+        context, MaterialPageRoute(builder: (context) => const Home()));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        // ignore: avoid_unnecessary_containers
+        child: Container(
+          child: const Text('Splash Screen',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+        ),
+      ),
+    );
+  }
+}
